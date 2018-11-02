4c8a3f54fe550e642dba1f01641bb97f46b43219
trzsroo <33383490+trzsroo@users.noreply.github.com>
2018-11-02 11:38:41 -0400

    Add files via upload
    changes

--- Client.py
+++ Client.py
@@ -1,1 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 09:50:11 2018
 
+@author: hillg2
+"""
+
+import socket
+serverHost = 'localHost'
+serverPort = 9999
+
+sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sockobj.connect((serverHost, serverPort))
+
+message = input()
+
+for line in message :
+    print('User 1: ', message)
+    sockobj.send(line.encode())
+    data = sockobj.recv(1024).decode()
+    print('User 2: ', str(data))
+
+sockobj.close()
